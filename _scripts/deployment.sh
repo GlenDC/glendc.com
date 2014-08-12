b53fa7bd129f94da67cfefec259eb68506dac532
glendc <decauwsemaecker.glen@gmail.com>
2014-08-12 00:23:37 +0000

    completing online resume

--- _scripts/deployment.sh
+++ _scripts/deployment.sh
@@ -3,17 +3,16 @@
 # Printable CV Generation
 
 file_resume=resume.md
-resume_dir=../_includes
+include_dir=../_includes
 
-cat $resume_dir/resume-header.md > $file_resume
-sed 's/[\t]//g' $resume_dir/resume-experience.md >> $file_resume
-cat $resume_dir/resume-skills.md >> $file_resume
-cat $resume_dir/resume-software.md >> $file_resume
-
-# hackatons ignored for now
-# cat $resume_dir/resume-hackatons.md >> $file_resume
-
-sed 's/[\t]//g' $resume_dir/resume-education.md >> $file_resume
+cat $include_dir/resume-header.md > $file_resume
+cat $include_dir/description.md >> $file_resume
+echo  >> $file_resume
+sed 's/[\t]//g' $include_dir/resume-experience.md >> $file_resume
+cat $include_dir/resume-skills.md >> $file_resume
+cat $include_dir/resume-software.md >> $file_resume
+cat $include_dir/resume-hackatons.md >> $file_resume
+sed 's/[\t]//g' $include_dir/resume-education.md >> $file_resume
 
 gimli -outputdir ../files/
 
