bda09dd21212d874e5931b07effef7f1da026bf9
glendc <decauwsemaecker.glen@gmail.com>
2014-09-07 19:43:10 +0000

    videos are now embed video a video ruby plugin

---
+++ _plugins/video.rb
@@ -0,0 +1,43 @@
+module Jekyll
+
+	# used to embed videos
+
+	class VideoTag < Liquid::Tag
+
+		def initialize(tag, params, tokens)
+			@params = params
+			super
+		end
+
+		def look_up(context, name)
+			lookup = context
+
+			name.split(".").each do |value|
+				lookup = lookup[value]
+			end
+
+			lookup
+		end
+
+		def render(context)
+			output = super
+
+			arguments = @params.split(' ')
+			
+			host = look_up(context, arguments[0])
+			id = look_up(context, arguments[1])
+			width = arguments[2]
+			height = arguments[3]
+
+			if host == "YouTube"
+				"<iframe width=\"#{width}\" height=\"#{height}\" src=\"//www.youtube.com/embed/#{id}\" frameborder=\"0\" allowfullscreen></iframe>"
+			else
+				"Error: '#{host}' is an unknown video host!"
+			end
+		end
+
+	end
+
+end
+
+Liquid::Template.register_tag('video', Jekyll::VideoTag)
