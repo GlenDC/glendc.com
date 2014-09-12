0db91f77d224ba5a5058e2664dbdbc4aa4ae251f
glendc <decauwsemaecker.glen@gmail.com>
2014-09-12 00:28:09 +0000

    deployment is simpler and adding pdf to git

--- _scripts/deployment.sh
+++ _scripts/deployment.sh
@@ -8,24 +8,6 @@ cd $WEBSITE
 
 git pull origin HEAD
 
-# Generate CV
-
-file_resume=resume.md
-include_dir=_includes
-
-cat $include_dir/resume-header.md > $file_resume
-cat $include_dir/description.md >> $file_resume
-echo  >> $file_resume
-sed 's/[\t]//g' $include_dir/resume-experience.md >> $file_resume
-cat $include_dir/resume-skills.md >> $file_resume
-cat $include_dir/resume-software.md >> $file_resume
-cat $include_dir/resume-hackatons.md >> $file_resume
-sed 's/[\t]//g' $include_dir/resume-education.md >> $file_resume
-
-gimli -outputdir files/ -f $file_resume -s /scripts/github.css
-
-sleep 10
-
 # generate new css
 
 sass --watch _sass:assets/css&
---
+++ _scripts/generate-resume.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Go to website root
+
+cd $WEBSITE
+
+# Generate CV
+
+file_resume=resume.md
+include_dir=_includes
+
+cat $include_dir/resume-header.md > $file_resume
+cat $include_dir/description.md >> $file_resume
+echo  >> $file_resume
+sed 's/[\t]//g' $include_dir/resume-experience.md >> $file_resume
+cat $include_dir/resume-skills.md >> $file_resume
+cat $include_dir/resume-software.md >> $file_resume
+cat $include_dir/resume-hackatons.md >> $file_resume
+sed 's/[\t]//g' $include_dir/resume-education.md >> $file_resume
+
+gimli -outputdir files/ -f $file_resume -s /scripts/github.css
+
+rm -f $file_resume
