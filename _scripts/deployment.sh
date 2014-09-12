d14878a235518fdf13f6adb9385a4f99d4d2ff51
glendc <decauwsemaecker.glen@gmail.com>
2014-09-12 00:23:24 +0000

    deployment resume update

--- _scripts/deployment.sh
+++ _scripts/deployment.sh
@@ -22,7 +22,7 @@ cat $include_dir/resume-software.md >> $file_resume
 cat $include_dir/resume-hackatons.md >> $file_resume
 sed 's/[\t]//g' $include_dir/resume-education.md >> $file_resume
 
-gimli -outputdir files/ && rm -f $file_resume
+gimli -outputdir files/ -f $file_resume -s /scripts/github.css
 
 sleep 10
 
